c174596d677053ce24512cfaf01a76f957ea62ae
MonstyrSlayr <59293011+MonstyrSlayr@users.noreply.github.com>
2022-10-11 07:51:26 -0400

    Update script.js

--- bicolage/project/script.js
+++ bicolage/project/script.js
@@ -86,7 +86,6 @@ for (var i = 0; i < srcArr.length; i++)
             sounds.push(new Howl
                 ({
                     src: newAud.snd,
-                    html5: true,
                 }));
 
             newAud.onclick = function()
@@ -110,6 +109,7 @@ let cityArr =
     new Source("img/2-0.jpg",       "img"),
     new Source("img/2-1.jpg",       "img"),
     new Source("img/2-2.jpg",       "img"),
+    new Source("audio/afroArtAmbiance.wav",                  "aud"),
     new Source("img/2-3.jpg",       "img"),
     new Source("img/2-4.jpg",       "img"),
     new Source("<p>\“You go to these lands and take dominion on those lands and the savages that live on these lands and you are to convert the savages to Christianity,” said Zunigha, summarizing the practical implementation of the doctrine. “Those that won't convert, you kill.\”</p>",
@@ -117,18 +117,21 @@ let cityArr =
     new Source("img/2-5.jpg",       "img"),
     new Source("img/2-6.jpg",       "img"),
     new Source("img/2-7.jpg",       "img"),
+    new Source("audio/preaching.wav",                  "aud"),
     new Source("img/2-8.jpg",       "img"),
     new Source("img/2-9.jpg",       "img"),
     new Source("img/2-10.jpg",      "img"),
     new Source("<p>Charles Dickens, American Notes for General Circulation (1842):</p> <p>\"There is one quarter, commonly called the Five Points, which in respect of filth and wretchedness, may be safely backed against Seven Dials, or any other part of famed St. Gile’s [a London slum]... These narrow ways, diverging to the right and left, and reeking everywhere with dirt and filth. Such lives as are led here, bear the same fruits here as elsewhere. The coarse and bloated faces at the doors, have counterparts at home, and all the wide world over. Debauchery has made the very houses prematurely old. See how the rotten beams are tumbling down, and how the patched and broken windows seem to scowl dimly, like eyes that have been hurt in drunken frays. Many of those pigs live here. Do they ever wonder why their masters walk upright in lieu of going on all fours? and why they talk instead of grunting?\"</p>",
                         "txt"),
     new Source("img/2-11.jpg",      "img"),
+    new Source("audio/spookyAmbiance.wav",                  "aud"),
     new Source("img/2-12.jpg",      "img"),
     new Source("img/2-13.jpg",      "img"),
     new Source("img/2-14.jpg",      "img"),
     new Source("<p>Rcvd. 7 November 1626</p> <p>High and Mighty Lords, Yesterday the ship the Arms of Amsterdam arrived here. It sailed from New Netherland out of the Hudson River on the 23rd of September. They report that our people are in good spirits and live in peace. The women also have born some children there. They have purchased the Island of Manhattan from the Natives for the value of 60 . It is about 22,000 acres in size. They had all their grain sowed by the middle of May and harvested by the middle of August. They sent samples of these summer grains: wheat, rye, barley, oats, buckwheat, canary seed, beans and flax. The cargo of the aforesaid ship is:</p> <ul> <li>7246 Beaver skins</li> <li>178½ Otter skins</li> <li>675 Otter skins</li> <li>48 Mink skins</li> <li>36 Lynx skins</li> <li>33 Minks</li> <li>34 Muskrat skins</li> </ul> <p>Many oak timbers and nut wood. Herewith, High and Mighty Lords, be commended to the mercy of the Almighty, In Amsterdam, the 5th of November, 1626.</p> <p>Your High and Mightinesses' obedient,</p> <p>P. Schaghen</p>",
                         "txt"),
     new Source("img/2-15.jpg",      "img"),
+    new Source("audio/spookyStrings.wav",                  "aud"),
     new Source("img/2-16.jpg",      "img"),
 ]
 
@@ -165,6 +168,34 @@ for (var i = 0; i < cityArr.length; i++)
             newTxt.innerHTML = cityArr[i].link;
 
             cityDiv.append(newTxt);
+            break;
+        }
+
+        case "aud":
+        {
+            let newAud = document.createElement("button");
+
+            newAud.className = "amstudImg";
+            newAud.style.width = 20 + "vh";
+            newAud.style.height = 20 + "vh";
+            newAud.innerHTML = "Click to play audio";
+            newAud.snd = cityArr[i].link;
+            newAud.id = sounds.length;
+            sounds.push(new Howl
+                ({
+                    src: newAud.snd,
+                }));
+
+            newAud.onclick = function()
+            {
+                for (var j = 0; j < sounds.length; j++)
+                {
+                    sounds[j].stop();
+                }
+                sounds[newAud.id].play();
+            }
+
+            cityDiv.append(newAud);
             break;
         }
     }
