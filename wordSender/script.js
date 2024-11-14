6a7c24303c3cbb83c39eec49d699733cb41dad9b
MonstyrSlayr <eehikioya0@gmail.com>
2024-11-14 11:22:21 -0500

    msm random monster generator 4.5.2

--- wordSender/script.js
+++ wordSender/script.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
+import { getDatabase, ref, set, get } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,6 +18,20 @@ const firebaseConfig =
 	appId: "1:1064096463566:web:e2a1d5ecb61689e041ae9f",
 	measurementId: "G-81K8K26S09"
 };
+
+const administrators = [1731525003400];
+let isAdmin = false;
+
+function writeUserData(userId)
+{
+	set(ref(daDatabase, 'users/' + userId),
+	{
+		username: "",
+		isMuted: false,
+		isBanned: false,
+		isAdmin: false
+  	});
+}
   
 // Initialize Firebase
 initializeApp(firebaseConfig);
@@ -32,7 +46,25 @@ document.getElementById("wordForm").addEventListener("submit", function(e)
 	const daWord = document.getElementById("wordInput").value;
 
 	const now = new Date();
-	const timeString = now.toLocaleTimeString(); 
+	const timeString = now.toLocaleTimeString();
+
+	if (getCookie("userId") == null)
+	{
+		setCookie("userId", now.getTime(), 364);
+	}
+	const userId = getCookie("userId");
+
+	get(ref(daDatabase, 'users/' + userId)).then((snapshot) =>
+	{
+		if (!snapshot.exists())
+		{
+			writeUserData(userId);
+		}
+		else
+		{
+			
+		}
+	})
 
 	const isSilent = isSilentCheckbox.checked;
 
@@ -42,6 +74,7 @@ document.getElementById("wordForm").addEventListener("submit", function(e)
 		word: daWord,
 		silent: isSilent,
 		time: timeString,
+		userId: userId,
 	});
 
 	// Clear input field
