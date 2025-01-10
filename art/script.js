7e18663f949c0e44b64aea1dd3471b84819730df
MonstyrSlayr <eehikioya0@gmail.com>
2025-01-10 11:42:17 -0500

    artwork data

---
+++ art/script.js
@@ -0,0 +1,9 @@
+// loads all of the artwork
+// like, ALL of the artwork
+// sorry
+
+const animations =
+[
+    "https://monstyrslayr.github.io/art/matchaWitchNicole.gif",
+    "https://monstyrslayr.github.io/art/mastermindAvery.gif"
+]
