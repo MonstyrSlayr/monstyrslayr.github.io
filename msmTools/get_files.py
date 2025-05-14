2d917e26f5b6f64649ecfb0aee417656407d275b
MonstyrSlayr <eehikioya0@gmail.com>
2025-05-14 15:31:15 -0400

    plasma titansoul

--- msmTools/get_files.py
+++ msmTools/get_files.py
@@ -6,6 +6,12 @@ def list_files(directory, output_file):
     
     # Filter out files that end with "_black.avif"
     filtered_files = [file for file in files if not file.endswith('_black.avif')]
+    filtered_files = [file for file in filtered_files if not file.endswith('_copy.avif')]
+    filtered_files = [file for file in filtered_files if not file.endswith('_gold_air.avif')]
+    filtered_files = [file for file in filtered_files if not file.endswith('_gold_cold.avif')]
+    filtered_files = [file for file in filtered_files if not file.endswith('_gold_earth.avif')]
+    filtered_files = [file for file in filtered_files if not file.endswith('_gold_plant.avif')]
+    filtered_files = [file for file in filtered_files if not file.endswith('_gold_water.avif')]
     
     # Write the filtered list of files to the output file
     with open(output_file, 'w') as f:
@@ -13,8 +19,8 @@ def list_files(directory, output_file):
             f.write(f"{file}\n")
 
 # Specify the directory path and output file path
-directory_path = './img'
-output_file_path = 'monsterImgs.txt'
+directory_path = './msmTools/img/portrait'
+output_file_path = './msmTools/monsterImgs.txt'
 
 # Call the function to list files and export them to a txt file
 list_files(directory_path, output_file_path)
