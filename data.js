82c205f3af67ed3ae566347b556a93dc7d5208ff
MonstyrSlayr <eehikioya0@gmail.com>
2026-04-22 18:14:46 -0400

    filters

--- data.js
+++ data.js
@@ -1,6 +1,53 @@
+function isLiveServer()
+{
+    return location.hostname === "127.0.0.1" || location.hostname === "localhost";
+}
+
+export const HOME = isLiveServer() ? "http://127.0.0.1:5500/" : "https://monstyrslayr.github.io/";
+
+export const FILTERTYPE = Object.freeze({
+    LANGUAGE: "Language", // python, html, etc.
+    DOMAIN: "Domain", // deltarune, juggling club, etc.
+    WEBSITE: "Host", // github, youtube, etc.
+    SCHOOL: "School" // masters, rit, etc.
+});
+
+export const filters = [];
+
+class Filter
+{
+    constructor(type, name, image = "")
+    {
+        this.type = type;
+        this.name = name;
+        this.image = image;
+        filters.push(this);
+    }
+}
+
+const masters = new Filter(FILTERTYPE.SCHOOL, "The Masters School");
+const rit = new Filter(FILTERTYPE.SCHOOL, "RIT");
+
+const python = new Filter(FILTERTYPE.LANGUAGE, "Python");
+const html = new Filter(FILTERTYPE.LANGUAGE, "HTML");
+const css = new Filter(FILTERTYPE.LANGUAGE, "CSS");
+const javascript = new Filter(FILTERTYPE.LANGUAGE, "JavaScript");
+const gml = new Filter(FILTERTYPE.LANGUAGE, "GameMaker Language");
+
+const juggling = new Filter(FILTERTYPE.DOMAIN, "Juggling Club");
+const deltarune = new Filter(FILTERTYPE.DOMAIN, "UNDERTALE / DELTARUNE");
+const brawlstars = new Filter(FILTERTYPE.DOMAIN, "Brawl Stars");
+const rhythmdoctor = new Filter(FILTERTYPE.DOMAIN, "Rhythm Doctor");
+const msm = new Filter(FILTERTYPE.DOMAIN, "My Singing Monsters");
+const lbal = new Filter(FILTERTYPE.DOMAIN, "Luck be a Landlord");
+const badapple = new Filter(FILTERTYPE.DOMAIN, "Bad Apple");
+
+const youtube = new Filter(FILTERTYPE.WEBSITE, "YouTube");
+const github = new Filter(FILTERTYPE.WEBSITE, "GitHub");
+
 class Project
 {
-    constructor(title, description, image, link, date, isWIP = false)
+    constructor(title, description, image, link, date, isWIP = false, filters = [])
     {
         this.title = title;
         this.desc = description;
@@ -8,188 +55,228 @@ class Project
         this.link = link;
         this.date = date;
         this.isWIP = isWIP;
+        this.filters = filters;
     }
 }
 
 const bicolage = new Project(
     "Bicolage",
     "I created this website along with 3 of my classmates in order to tell our American Studies class about the history of Seneca Village.",
-    "https://monstyrslayr.github.io/img/bicolage.png",
+    HOME + "img/bicolage.png",
     "https://monstyrslayr.github.io/bicolage/project/",
-    new Date("02/01/2023")
+    new Date("02/01/2023"),
+    false,
+    [masters, html, css]
 );
 
 const boxPush = new Project(
     "boxPush",
     "I created a game using GameMaker Studio 2 for my final project in my physics mechanics class. Two opponents push a box into their goals.",
-    "https://monstyrslayr.github.io/img/boxPush.png",
+    HOME + "img/boxPush.png",
     "https://monstyrslayr.github.io/boxPush/",
-    new Date("01/16/2023")
+    new Date("01/16/2023"),
+    false,
+    [masters, gml]
 );
 
 const magonet = new Project(
     "Magonet",
     "I created a game using GameMaker Studio for my final project in my physics electricity & magnetism class. Guide your magnetic character into the yellow goals.",
-    "https://monstyrslayr.github.io/img/magoNet.png",
+    HOME + "img/magoNet.png",
     "https://monstyrslayr.github.io/magonet/",
-    new Date("05/24/2023")
+    new Date("05/24/2023"),
+    false,
+    [masters, gml]
 );
 
 const lbaldle = new Project(
     "LBALdle",
-    "This is a passion project based on Wordle, the popular word guessing game, and Luck Be a Landlord, one of my favorite video games. You are able to guess a Luck Be a Landlord symbol based on clues.",
-    "https://monstyrslayr.github.io/img/LBALDLE.png",
+    "This is a passion project based on Wordle, the popular word guessing game, and Luck be a Landlord, one of my favorite video games. You are able to guess a Luck be a Landlord symbol based on clues.",
+    HOME + "img/LBALDLE.png",
     "https://monstyrslayr.github.io/lbaldle/",
-    new Date("09/22/2024")
+    new Date("09/22/2024"),
+    false,
+    [html, css, javascript, lbal]
 );
 
 const tigerGoodbuy = new Project(
     "Tiger Goodbuy",
     "A team of students and I created a prototype for a website similar to Ebay, but for RIT students.",
-    "https://monstyrslayr.github.io/img/tigerGoodbuy.png",
+    HOME + "img/tigerGoodbuy.png",
     "https://adeseyn.github.io/tigergoodbuy/",
-    new Date("12/09/2024")
+    new Date("12/09/2024"),
+    false,
+    [rit, html, css, javascript]
 );
 
 const msmRMG = new Project(
     "MSM Random Monster Generator",
     "I created a website for fans of My Singing Monsters, the hit mobile game. This site generates a random monster from the game. All of the other generators I found were outdated, so I made my own!",
-    "https://monstyrslayr.github.io/img/MSMRMG.png",
+    HOME + "img/MSMRMG.png",
     "https://monstyrslayr.github.io/randomMonsterGenerator/",
-    new Date("06/12/2025")
+    new Date("06/12/2025"),
+    false,
+    [html, css, javascript, msm]
 );
 
 const ameliorateWiki = new Project(
     "Ameliorate Wiki",
     "For my YouTube fans to explore the extensive lore of my original characters.",
-    "https://monstyrslayr.github.io/img/ameWiki.png",
+    HOME + "img/ameWiki.png",
     "https://monstyrslayr.github.io/wiki/",
     new Date("09/20/25"),
-    true
+    true,
+    [html, css, javascript, msm]
 );
 
 const msmIncredibox = new Project(
     "MSM Incredibox",
     "A passion project mashup of Incredibox mechanics and My Singing Monsters music.",
-    "https://monstyrslayr.github.io/img/msmincredibox.png",
+    HOME + "img/msmincredibox.png",
     "https://monstyrslayr.github.io/msmincredibox/",
     new Date("04/10/2025"),
-    true
+    true,
+    [html, css, javascript, msm]
 );
 
 const lbalBadApple = new Project(
     "Bad Apple but it's Luck be a Landlord",
     "A recreation of the Bad Apple music video using nothing but Python and LbaL symbols. Remaster of a January 2024 project.",
-    "https://monstyrslayr.github.io/img/lbalBadApple.png",
+    HOME + "img/lbalBadApple.png",
     "https://github.com/MonstyrSlayr/lbalBadApple",
-    new Date("05/27/2025")
+    new Date("05/27/2025"),
+    false,
+    [python, lbal, badapple, github, youtube]
 );
 
 const msmMemory = new Project(
     "MSM Ultimate Memory Game",
     "Let's see how well My Singing Monsters fans can name every single monster in the game.",
-    "https://monstyrslayr.github.io/img/msmMemoryGame.png",
+    HOME + "img/msmMemoryGame.png",
     "https://monstyrslayr.github.io/ultimateMemoryGame/",
-    new Date("06/13/2025")
+    new Date("06/13/2025"),
+    false,
+    [html, css, javascript, msm]
 );
 
 const msmChainer = new Project(
     "MSM Monster Chains",
     "Make a monster chain that links two monsters through their inhabited islands!",
-    "https://monstyrslayr.github.io/img/monsterChainer.png",
+    HOME + "img/monsterChainer.png",
     "https://monstyrslayr.github.io/monsterChains/",
-    new Date("07/28/2025")
+    new Date("07/28/2025"),
+    false,
+    [html, css, javascript, msm]
 );
 
 const msmSudoku = new Project(
     "MSM Sudoku",
     "Make and play MSM Sudokus made my My Singing Monsters fans!",
-    "https://monstyrslayr.github.io/img/msmSudoku.png",
+    HOME + "img/msmSudoku.png",
     "https://monstyrslayr.github.io/msmsudoku/",
-    new Date("10/10/2025")
+    new Date("10/10/2025"),
+    false,
+    [html, css, javascript, msm]
 );
 
 const msmGuessWho = new Project(
     "MSM Guess Who",
     "Guess the My Singing Monster based on the clues!",
-    "https://monstyrslayr.github.io/img/msmGuessWho.png",
+    HOME + "img/msmGuessWho.png",
     "https://monstyrslayr.github.io/msmGuessWho/",
-    new Date("11/09/2025")
+    new Date("11/09/2025"),
+    false,
+    [html, css, javascript, msm]
 );
 
 const blackCatSaga = new Project(
     "Black Cat Saga",
     "Black Cat Island is the first animated island I have ever created, and thus, it holds a special place in my heart. Most of the monsters are based on cats.",
-    "https://monstyrslayr.github.io/img/blackCat.png",
+    HOME + "img/blackCat.png",
     "https://youtu.be/RYFg599fCRM",
-    new Date("11/14/2020")
+    new Date("11/14/2020"),
+    false,
+    [youtube]
 );
 
 const ameliorateSaga = new Project(
     "Ameliorate Saga",
     "I started Signal Stadium around a year after my old laptop crashed and I lost all of my intricate animation projects, including the Black Cat Saga. This island helped me to get back into the flow of music and animation.",
-    "https://monstyrslayr.github.io/img/sigStad.png",
+    HOME + "img/sigStad.png",
     "https://youtu.be/x03o46Deeyo",
     new Date("05/19/2025"), // do something you fool
-    true
+    true,
+    [youtube]
 );
 
 const artArchive = new Project(
     "General Art Archive",
     "I'm trying a new way to share my general / uncategorized artwork.",
-    "https://monstyrslayr.github.io/img/artSite.png",
+    HOME + "img/artSite.png",
     "https://monstyrslayr.github.io/art",
     new Date("03/20/25"),
-    true
+    true,
+    [html, css, javascript]
 );
 
 const fullRadiation = new Project(
     "Undertale 10th Anniversary - Toby Fox",
     "A program recreating the speech-to-text setup Toby Fox used during the UNDERTALE 10th Anniversary livestream.",
-    "https://monstyrslayr.github.io/img/fullRadiation.png",
+    HOME + "img/fullRadiation.png",
     "https://github.com/MonstyrSlayr/Undertale-10th-Anniversary-Toby-Fox",
-    new Date("10/4/2025")
+    new Date("10/4/2025"),
+    false,
+    [python, deltarune, github]
 );
 
 const deltaruneMotifs = new Project(
     "Deltarune Motifs",
     "A website documenting leitmotifs in the soundtracks of UNDERTALE and DELTARUNE.",
-    "https://monstyrslayr.github.io/img/deltaruneMotifs.png",
+    HOME + "img/deltaruneMotifs.png",
     "https://monstyrslayr.github.io/deltarunemotifs",
     new Date("12/20/2025"),
-    true
+    true,
+    [html, css, javascript, deltarune, github, youtube]
 );
 
 const bossFightBadApple = new Project(
     "Bad Apple but it's Rhythm Doctor - Boss Fight",
     "A recreation of the Bad Apple music video using the blinking eyes in the background of a Rhythm Doctor level.",
-    "https://monstyrslayr.github.io/img/rdBapple.png",
+    HOME + "img/rdBapple.png",
     "https://youtu.be/cyWGuhD37eQ",
-    new Date("1/14/2026")
+    new Date("1/14/2026"),
+    false,
+    [python, rhythmdoctor, badapple, youtube]
 );
 
 const kairosProTierLists = new Project(
     "KairosTime Pro Tier Lists Analysis",
     "A website collating the information from KairosTime's Professional Tier Lists to analyze trends in the video game Brawl Stars.",
-    "https://monstyrslayr.github.io/img/kairosTime.jpg",
+    HOME + "img/kairosTime.jpg",
     "https://monstyrslayr.github.io/kairosTimeProTierLists/",
-    new Date("3/17/2026")
+    new Date("3/17/2026"),
+    false,
+    [html, css, javascript, brawlstars]
 );
 
 const jugglingClubWebsite = new Project(
     "RIT Juggling Club Landing Page",
     "A website about RIT Juggling Club, hosted on Blogger",
-    "https://monstyrslayr.github.io/img/jugglingClub.png",
+    HOME + "img/jugglingClub.png",
     "https://ritjuggle.blogspot.com/2025/11/landing-page.html",
-    new Date("11/05/2025")
+    new Date("11/05/2025"),
+    false,
+    [html, css, javascript, juggling]
 );
 
 const juggleInWebsite = new Project(
     "46th Annual Spring Juggle-In Information Page",
     "A website containing information about RIT Juggling Club's 46th Annual Juggle-In, hosted on Blogger",
-    "https://monstyrslayr.github.io/img/juggleIn.png",
+    HOME + "img/juggleIn.png",
     "https://ritjuggle.blogspot.com/2026/02/rit-46th-annual-spring-juggle-in.html",
-    new Date("4/12/2026")
+    new Date("4/12/2026"),
+    false,
+    [html, css, javascript, juggling]
 );
 
 export const programmingProjects =
--- programming/programming.js
+++ programming/programming.js
@@ -1,4 +1,4 @@
-import { createNavbar, programmingProjects, createFooter, createProjectDiv, createRandomTransitionIn, createRandomTransitionOut, createImageQuartet, scrollQuartets, featuredProgrammingProject } from "../data.js";
+import { createNavbar, programmingProjects, createFooter, createProjectDiv, createRandomTransitionIn, createRandomTransitionOut, createImageQuartet, scrollQuartets, featuredProgrammingProject, filters, FILTERTYPE } from "../data.js";
 
 const header = document.createElement("header");
 document.body.appendChild(header);
@@ -30,62 +30,148 @@ const diaTransitionDiv = document.createElement("div");
 diaTransitionDiv.classList.add("diaTransitionDivIn");
 header.appendChild(diaTransitionDiv);
 
+function repostProgrammingProjects(filter = null)
+{
+    wrappersWrapper.innerHTML = "";
+    if (document.getElementsByTagName("footer").length > 0) document.getElementsByTagName("footer")[0].remove();
+    if (document.getElementById("footerPadder")) document.getElementById("footerPadder").remove();
+
+    let numProjects = 0;
+    for (let i = 0; i < programmingProjects.length; i++)
+    {
+        const project = programmingProjects[i];
+
+        if (!project.filters.includes(filter) && filter != null) continue;
+        numProjects++;
+
+        const projectWrapper = document.createElement("div"); // necessary for background shenanigans
+        projectWrapper.classList.add("projectWrapper");
+        wrappersWrapper.appendChild(projectWrapper);
+
+        const projectDiv = createProjectDiv(project);
+
+        if (numProjects % 2 == 0) // white bg
+        {
+            const daPaddingDiv = document.createElement("div");
+            daPaddingDiv.classList.add("paddingDiv");
+            projectWrapper.appendChild(daPaddingDiv);
+
+                daPaddingDiv.appendChild(projectDiv);
+        }
+        else // image quartet bg
+        {
+            projectWrapper.appendChild(createRandomTransitionOut());
+
+            projectWrapper.appendChild(projectDiv);
+
+            projectWrapper.appendChild(createRandomTransitionIn());
+            
+            const daImageQuartet = createImageQuartet();
+            projectWrapper.appendChild(daImageQuartet);
+        }
+    }
+
+    const footer = createFooter();
+
+    if (numProjects % 2 == 1)
+    {
+        const paddingDiv = document.createElement("div");
+        paddingDiv.classList.add("paddingDiv");
+        paddingDiv.id = "footerPadder";
+        document.body.appendChild(paddingDiv);
+
+        paddingDiv.appendChild(footer);
+    }
+    else
+    {
+        document.body.appendChild(footer);
+    }
+}
+
 const paddingDiv = document.createElement("div");
 paddingDiv.classList.add("paddingDiv");
 document.body.appendChild(paddingDiv);
 
     const desc = document.createElement("p");
-    desc.textContent = `
-        Here are some programming projects I've worked on!
-    `
+    desc.textContent = "Here are some programming projects I've worked on!";
     paddingDiv.appendChild(desc);
+
+    const filterHeading = document.createElement("h2");
+    filterHeading.textContent = "Filter";
+    paddingDiv.appendChild(filterHeading);
+
+    const filtersDiv = document.createElement("div");
+    filtersDiv.classList.add("filtersDiv");
+    paddingDiv.appendChild(filtersDiv);
+
+        const filtersButton = document.createElement("button");
+        filtersButton.textContent = "Filter by:";
+        filtersDiv.appendChild(filtersButton);
+
+        filtersButton.addEventListener("click", () =>
+        {
+            filtersList.classList.toggle("show");
+        });
+
+        window.addEventListener("click", (e) =>
+        {
+            if (filtersButton.contains(e.target)) return;
+            filtersList.classList.remove("show");
+        });
+
+        const filtersList = document.createElement("div");
+        filtersList.classList.add("filtersList");
+        filtersDiv.appendChild(filtersList);
+
+        // ALL
+        const filterSelectorAll = document.createElement("div");
+        filterSelectorAll.classList.add("filterSelector");
+        filtersList.appendChild(filterSelectorAll);
+
+        filterSelectorAll.addEventListener("click", () =>
+        {
+            repostProgrammingProjects();
+            filtersList.classList.remove("show");
+        });
+
+            const filterAllP = document.createElement("p");
+            filterAllP.textContent = "All";
+            filterSelectorAll.appendChild(filterAllP);
+
+        for (const h of Object.values(FILTERTYPE))
+        {
+            const filterHeader = document.createElement("div");
+            filterHeader.classList.add("filterHeader");
+            filtersList.appendChild(filterHeader);
+
+                const filterH = document.createElement("h2");
+                filterH.textContent = h;
+                filterHeader.appendChild(filterH);
+            
+            // for every filter of this type that is in at least one project
+            for (const f of filters.filter(fi => fi.type == h && programmingProjects.some(p => p.filters.includes(fi))))
+            {
+                const filterSelector = document.createElement("div");
+                filterSelector.classList.add("filterSelector");
+                filtersList.appendChild(filterSelector);
+
+                filterSelector.addEventListener("click", () =>
+                {
+                    repostProgrammingProjects(f);
+                    filtersList.classList.remove("show");
+                });
+
+                    const filterP = document.createElement("p");
+                    filterP.textContent = f.name;
+                    filterSelector.appendChild(filterP);
+            }
+        }
 
 programmingProjects.sort((a, b) => b.date - a.date); // chronological desc
 
-for (let i = 0; i < programmingProjects.length; i++)
-{
-    const project = programmingProjects[i];
-
-    const projectWrapper = document.createElement("div"); // necessary for background shenanigans
-    projectWrapper.classList.add("projectWrapper");
-    document.body.appendChild(projectWrapper);
-
-    const projectDiv = createProjectDiv(project);
-
-    if (i % 2 == 1) // white bg
-    {
-        const daPaddingDiv = document.createElement("div");
-        daPaddingDiv.classList.add("paddingDiv");
-        projectWrapper.appendChild(daPaddingDiv);
-
-            daPaddingDiv.appendChild(projectDiv);
-    }
-    else // image quartet bg
-    {
-        projectWrapper.appendChild(createRandomTransitionOut());
-
-        projectWrapper.appendChild(projectDiv);
-
-        projectWrapper.appendChild(createRandomTransitionIn());
-        
-        const daImageQuartet = createImageQuartet();
-        projectWrapper.appendChild(daImageQuartet);
-    }
-}
-
-const footer = createFooter();
-
-if (programmingProjects.length % 2 == 1)
-{
-    const paddingDiv = document.createElement("div");
-    paddingDiv.classList.add("paddingDiv");
-    document.body.appendChild(paddingDiv);
-
-    paddingDiv.appendChild(footer);
-}
-else
-{
-    document.body.appendChild(footer);
-}
+const wrappersWrapper = document.createElement("div");
+document.body.appendChild(wrappersWrapper);
 
 scrollQuartets();
+
+repostProgrammingProjects();
